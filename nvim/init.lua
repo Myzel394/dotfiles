985b7ab2257e202031d5576d6b9b0697756d8382
Myzel394 <50424412+Myzel394@users.noreply.github.com>
2023-10-16 19:54:22 +0200

    feat: Add SmoothCursor

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,6 +1,29 @@
 require("configs")
 
-vim.g.copilot_filetypes = {markdown = true}
-vim.g.coc_filetype_map = {markdown = true}
+vim.g.copilot_filetypes = { markdown = true }
+vim.g.coc_filetype_map = { markdown = true }
 
 
+local autocmd = vim.api.nvim_create_autocmd
+
+autocmd({ 'ModeChanged' }, {
+    callback = function()
+        local current_mode = vim.fn.mode()
+        if current_mode == 'n' then
+            vim.api.nvim_set_hl(0, 'SmoothCursor', { fg = '#8aa872' })
+            vim.fn.sign_define('smoothcursor', { text = '▷' })
+        elseif current_mode == 'v' then
+            vim.api.nvim_set_hl(0, 'SmoothCursor', { fg = '#bf616a' })
+            vim.fn.sign_define('smoothcursor', { text = '' })
+        elseif current_mode == 'V' then
+            vim.api.nvim_set_hl(0, 'SmoothCursor', { fg = '#bf616a' })
+            vim.fn.sign_define('smoothcursor', { text = '' })
+        elseif current_mode == '�' then
+            vim.api.nvim_set_hl(0, 'SmoothCursor', { fg = '#bf616a' })
+            vim.fn.sign_define('smoothcursor', { text = '' })
+        elseif current_mode == 'i' then
+            vim.api.nvim_set_hl(0, 'SmoothCursor', { fg = '#668aab' })
+            vim.fn.sign_define('smoothcursor', { text = '' })
+        end
+    end,
+})
