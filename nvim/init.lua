86ee8e59502692ee023f29977871d79940d96df2
Myzel394 <50424412+Myzel394@users.noreply.github.com>
2023-10-16 17:58:27 +0000

    fix: Allow copilot also on yaml

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,6 +1,6 @@
 require("configs")
 
-vim.g.copilot_filetypes = {markdown = true}
-vim.g.coc_filetype_map = {markdown = true}
+vim.g.copilot_filetypes = {markdown = true, yaml = true, yml = true}
+vim.g.coc_filetype_map = {markdown = true, yaml = true, yml = true}
 
 
