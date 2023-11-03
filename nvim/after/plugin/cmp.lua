a2a9fa2a441245872f6b3bd6f20731fef282631a
Myzel394 <50424412+Myzel394@users.noreply.github.com>
2023-11-03 21:02:23 +0100

    current stand: Adding luasnip

--- nvim/after/plugin/cmp.lua
+++ nvim/after/plugin/cmp.lua
@@ -6,10 +6,16 @@ cmp.setup({
         ['<Tab>'] = cmp.mapping.confirm({ select = false }),
         ['<C-Space>'] = cmp.mapping.complete(),
     }),
+    snippet = {
+        expand = function(args)
+            require 'luasnip'.lsp_expand(args.body)
+        end
+    },
     sources = {
         { name = 'path' },
         { name = 'nvim_lsp' },
         { name = 'nvim_lua' },
+        { name = 'luasnip', option = { show_autosnippets = true } },
     },
     formatting = { format = require("tailwindcss-colorizer-cmp").formatter },
 })
