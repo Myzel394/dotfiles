9bdb293db59688fc86cfeb3c68d7b71aecf8b11a
Myzel394 <50424412+Myzel394@users.noreply.github.com>
2023-11-09 10:29:00 +0100

    feat: Add dynamic theme from background image

---
+++ scripts/create_theme_from_bg_image.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+FOLDER=~/Pictures/BingWallpaper
+# Finds the most recent image in the folder
+IMAGE=$(ls "$FOLDER" | sort -k 1,1 -t"-" -n --reverse | head -n 1)
+
+node ~/.config/scripts/set_theme_from_bg.js "$FOLDER/$IMAGE"
+
