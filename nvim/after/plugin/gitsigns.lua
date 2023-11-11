051554f46423f98ad06f6f1f48d8186f3cbd0f8e
Myzel394 <50424412+Myzel394@users.noreply.github.com>
2023-11-11 18:39:24 +0100

    fix: Overall nvim improvements

--- nvim/after/plugin/gitsigns.lua
+++ nvim/after/plugin/gitsigns.lua
@@ -1,3 +1,9 @@
-require('gitsigns').setup {
-  current_line_blame = true,
-}
+require('gitsigns').setup {}
+
+vim.keymap.set("n", "<leader>gh", "<cmd>:Gitsigns preview_hunk<cr>", { desc = "Preview hunk" })
+vim.keymap.set("n", "<leader>gn", "<cmd>:Gitsigns next_hunk<cr>", { desc = "Next hunk" })
+vim.keymap.set("n", "<leader>gp", "<cmd>:Gitsigns prev_hunk<cr>", { desc = "Previous hunk" })
+vim.keymap.set("n", "<leader>gr", "<cmd>:Gitsigns reset_hunk<cr>", { desc = "Reset hunk" })
+vim.keymap.set("n", "<leader>gs", "<cmd>:Gitsigns stage_hunk<cr>", { desc = "Stage hunk" })
+vim.keymap.set("n", "<leader>gu", "<cmd>:Gitsigns undo_stage_hunk<cr>", { desc = "Undo stage hunk" })
+vim.keymap.set("n", "<leader>gb", "<cmd>:Gitsigns blame_line<cr>", { desc = "Blame line" })
--- nvim/after/plugin/indent-blankline.lua
+++ nvim/after/plugin/indent-blankline.lua
@@ -7,7 +7,6 @@ local highlight = {
     "RainbowViolet",
     "RainbowCyan",
 }
-
 local hooks = require "ibl.hooks"
 -- create the highlight groups in the highlight setup hook, so they are reset
 -- every time the colorscheme changes
@@ -21,4 +20,7 @@ hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
     vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#56B6C2" })
 end)
 
-require("ibl").setup { indent = { highlight = highlight } }
+vim.g.rainbow_delimiters = { highlight = highlight }
+require("ibl").setup { scope = { highlight = highlight } }
+
+hooks.register(hooks.type.SCOPE_HIGHLIGHT, hooks.builtin.scope_highlight_from_extmark)
