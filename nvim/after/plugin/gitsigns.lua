86814dc898dd8a4eb153691b4e599156a7ff4ddc
Myzel394 <50424412+Myzel394@users.noreply.github.com>
2023-11-12 20:51:56 +0100

    fix: Overall nvim improvements

--- nvim/after/plugin/gitsigns.lua
+++ nvim/after/plugin/gitsigns.lua
@@ -1,10 +1,10 @@
-require('gitsigns').setup {}
+require("gitsigns").setup {}
 
 vim.keymap.set("n", "<leader>gh", "<cmd>:Gitsigns preview_hunk<cr>", { desc = "Preview hunk" })
 vim.keymap.set("n", "<leader>gn", "<cmd>:Gitsigns next_hunk<cr>", { desc = "Next hunk" })
 vim.keymap.set("n", "<leader>gp", "<cmd>:Gitsigns prev_hunk<cr>", { desc = "Previous hunk" })
 vim.keymap.set("n", "<leader>gr", "<cmd>:Gitsigns reset_hunk<cr>", { desc = "Reset hunk" })
 vim.keymap.set("n", "<leader>gs", "<cmd>:Gitsigns stage_hunk<cr>", { desc = "Stage hunk" })
-vim.keymap.set("n", "<leader>gsb", "<cmd>:Gitsigns stage_buffer<cr>", { desc = "Stage buffer" })
+vim.keymap.set("n", "<leader>gb", "<cmd>:Gitsigns stage_buffer<cr>", { desc = "Stage buffer" })
 vim.keymap.set("n", "<leader>gu", "<cmd>:Gitsigns undo_stage_hunk<cr>", { desc = "Undo stage hunk" })
-vim.keymap.set("n", "<leader>gb", "<cmd>:Gitsigns blame_line<cr>", { desc = "Blame line" })
+vim.keymap.set("n", "<leader>gl", "<cmd>:Gitsigns blame_line<cr>", { desc = "Blame line" })
--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -1,5 +1,3 @@
-local null_ls = require("null-ls")
-local mason_null_ls = require("mason-null-ls")
 local lsp = require('lsp-zero').preset("recommended")
 
 lsp.on_attach(function(client, bufnr)
@@ -8,78 +6,211 @@ lsp.on_attach(function(client, bufnr)
     lsp.default_keymaps({ buffer = bufnr })
 end)
 
-require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
-
-lsp.setup_servers({
-    "tsserver",
-    "eslint",
-    "tailwindcss",
-    "dockerls",
-    "docker_compose_language_service"
-})
-
 lsp.setup()
 
-local function filterReactDTS(value)
-    return string.match(value.filename, "react/index.d.ts") == nil
+require("mason").setup({})
+require("mason-lspconfig").setup {
+    ensure_installed = {
+        "rust_analyzer",
+        "tsserver",
+        "dockerls",
+        "docker_compose_language_service",
+        "html",
+        "cssls",
+        "jsonls",
+        "yamlls",
+        "bashls",
+        "graphql",
+        "tailwindcss",
+        "lua_ls",
+    }
+}
+
+----- Lsp Config -----
+local lspconfig = require("lspconfig")
+local cmp_nvim_lsp = require("cmp_nvim_lsp")
+
+local on_attach = function(client, bufnr)
 end
 
--- custom list handler
-local function on_list(options)
-    local items = options.items
-    if #items > 1 then
-        -- filter out react/index.d.ts
-        items = filter(items, filterReactDTS)
-    end
+-- used to enable autocompletion (assign to every lsp server config)
+local capabilities = cmp_nvim_lsp.default_capabilities()
 
-    vim.fn.setqflist({}, " ", { title = options.title, items = items, context = options.context })
-    if #items > 1 then
-        -- if more than one option, open loc list
-        vim.api.nvim_command "copen"
-    else
-        -- else jump directly
-        vim.api.nvim_command "cfirst"
-    end
+-- Change the Diagnostic symbols in the sign column
+local signs = { Error = " ", Warn = " ", Hint = "󰠠 ", Info = " " }
+for type, icon in pairs(signs) do
+    local hl = "DiagnosticSign" .. type
+    vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
 end
 
-local null_opts = lsp.build_options("null-ls", {
-    on_attach = function(client, bufnr)
-        vim.api.nvim_create_autocmd("BufWritePre", {
-            desc = "Auto format before save",
-            pattern = "<buffer>",
-            callback = function()
-                vim.lsp.buf.format {
-                    filter = function()
-                        -- only use null-ls for formatting
-                        return client.name == "null-ls"
-                    end,
-                    bufnr = bufnr,
-                }
-            end,
-        })
-    end,
+-- configure html server
+lspconfig["html"].setup({
+    capabilities = capabilities,
+    on_attach = on_attach,
 })
 
-lsp.on_attach(function(client, bufnr)
-    local opts = { buffer = bufnr, remap = false }
+-- configure typescript server with plugin
+lspconfig["tsserver"].setup({
+    capabilities = capabilities,
+    on_attach = on_attach,
+})
 
-    vim.keymap.set("n", "gd", function()
-        vim.lsp.buf.definition { on_list = on_list }
-    end, opts)
-end)
+-- configure css server
+lspconfig["cssls"].setup({
+    capabilities = capabilities,
+    on_attach = on_attach,
+})
 
-null_ls.setup {
-    on_attach = null_opts.on_attach,
-    sources = {
-        -- null_ls.builtins.formatting.pg_format,
-        null_ls.builtins.formatting.prettierd,
-        null_ls.builtins.formatting.rustfmt
+-- configure tailwindcss server
+lspconfig["tailwindcss"].setup({
+    capabilities = capabilities,
+    on_attach = on_attach,
+})
+
+-- configure graphql language server
+lspconfig["graphql"].setup({
+    capabilities = capabilities,
+    on_attach = on_attach,
+    filetypes = { "graphql", "gql", "svelte", "typescriptreact", "javascriptreact" },
+})
+
+-- configure graphql language server
+lspconfig["bashls"].setup({
+    capabilities = capabilities,
+    on_attach = on_attach,
+})
+
+-- configure graphql language server
+lspconfig["jsonls"].setup({
+    capabilities = capabilities,
+    on_attach = on_attach,
+})
+
+-- configure graphql language server
+lspconfig["yamlls"].setup({
+    capabilities = capabilities,
+    on_attach = on_attach,
+})
+
+-- configure graphql language server
+lspconfig["dockerls"].setup({
+    capabilities = capabilities,
+    on_attach = on_attach,
+})
+
+-- configure graphql language server
+lspconfig["docker_compose_language_service"].setup({
+    capabilities = capabilities,
+    on_attach = on_attach,
+})
+
+-- configure graphql language server
+lspconfig["rust_analyzer"].setup({
+    capabilities = capabilities,
+    on_attach = on_attach,
+})
+
+-- configure lua server (with special settings)
+lspconfig["lua_ls"].setup({
+    capabilities = capabilities,
+    on_attach = on_attach,
+    settings = { -- custom settings for lua
+        Lua = {
+            -- make the language server recognize "vim" global
+            diagnostics = {
+                globals = { "vim" },
+            },
+            workspace = {
+                -- make language server aware of runtime files
+                library = {
+                    [vim.fn.expand("$VIMRUNTIME/lua")] = true,
+                    [vim.fn.stdpath("config") .. "/lua"] = true,
+                },
+            },
+        },
     },
-}
+})
 
-mason_null_ls.setup {
-    ensure_installed = nil,
-    automatic_installation = true,
-    automatic_setup = false,
-    handlers = {},
-}
+----- CMP -----
+
+local cmp = require("cmp")
+
+cmp.setup({
+    preselect = "item",
+    mapping = cmp.mapping.preset.insert({
+        ["<Tab>"] = cmp.mapping.confirm({ select = true }),
+        ["<C-Space>"] = cmp.mapping.complete(),
+        ["<CR>"] = cmp.mapping.confirm({ select = false })
+    }),
+    snippet = {
+        expand = function(args)
+            require "luasnip".lsp_expand(args.body)
+        end
+    },
+    sources = {
+        { name = "path" },
+        { name = "nvim_lsp" },
+        { name = "nvim_lua" },
+        { name = "luasnip", option = { show_autosnippets = true } },
+    },
+    window = {
+        completion = {
+            winhighlight = "Normal:Pmenu,FloatBorder:Pmenu,Search:None",
+            col_offset = -3,
+            side_padding = 0,
+        },
+    },
+    formatting = {
+        fields = { "kind", "abbr", "menu" },
+        format = function(entry, vim_item)
+            local kind = require("lspkind").cmp_format({ mode = "symbol_text", maxwidth = 50 })(entry, vim_item)
+            local strings = vim.split(kind.kind, "%s", { trimempty = true })
+            kind.kind = " " .. (strings[1] or "") .. " "
+            kind.menu = "    (" .. (strings[2] or "") .. ")"
+
+            return kind
+        end,
+    },
+})
+
+-- Customization for Pmenu
+vim.api.nvim_set_hl(0, "PmenuSel", { bg = "#282C34", fg = "NONE" })
+vim.api.nvim_set_hl(0, "Pmenu", { fg = "#C5CDD9", bg = "#22252A" })
+
+vim.api.nvim_set_hl(0, "CmpItemAbbrDeprecated", { fg = "#7E8294", bg = "NONE", strikethrough = true })
+vim.api.nvim_set_hl(0, "CmpItemAbbrMatch", { fg = "#82AAFF", bg = "NONE", bold = true })
+vim.api.nvim_set_hl(0, "CmpItemAbbrMatchFuzzy", { fg = "#82AAFF", bg = "NONE", bold = true })
+vim.api.nvim_set_hl(0, "CmpItemMenu", { fg = "#C792EA", bg = "NONE", italic = true })
+
+vim.api.nvim_set_hl(0, "CmpItemKindField", { fg = "#EED8DA", bg = "#B5585F" })
+vim.api.nvim_set_hl(0, "CmpItemKindProperty", { fg = "#EED8DA", bg = "#B5585F" })
+vim.api.nvim_set_hl(0, "CmpItemKindEvent", { fg = "#EED8DA", bg = "#B5585F" })
+
+vim.api.nvim_set_hl(0, "CmpItemKindText", { fg = "#C3E88D", bg = "#9FBD73" })
+vim.api.nvim_set_hl(0, "CmpItemKindEnum", { fg = "#C3E88D", bg = "#9FBD73" })
+vim.api.nvim_set_hl(0, "CmpItemKindKeyword", { fg = "#C3E88D", bg = "#9FBD73" })
+
+vim.api.nvim_set_hl(0, "CmpItemKindConstant", { fg = "#FFE082", bg = "#D4BB6C" })
+vim.api.nvim_set_hl(0, "CmpItemKindConstructor", { fg = "#FFE082", bg = "#D4BB6C" })
+vim.api.nvim_set_hl(0, "CmpItemKindReference", { fg = "#FFE082", bg = "#D4BB6C" })
+
+vim.api.nvim_set_hl(0, "CmpItemKindFunction", { fg = "#EADFF0", bg = "#A377BF" })
+vim.api.nvim_set_hl(0, "CmpItemKindStruct", { fg = "#EADFF0", bg = "#A377BF" })
+vim.api.nvim_set_hl(0, "CmpItemKindClass", { fg = "#EADFF0", bg = "#A377BF" })
+vim.api.nvim_set_hl(0, "CmpItemKindModule", { fg = "#EADFF0", bg = "#A377BF" })
+vim.api.nvim_set_hl(0, "CmpItemKindOperator", { fg = "#EADFF0", bg = "#A377BF" })
+
+vim.api.nvim_set_hl(0, "CmpItemKindVariable", { fg = "#C5CDD9", bg = "#7E8294" })
+vim.api.nvim_set_hl(0, "CmpItemKindFile", { fg = "#C5CDD9", bg = "#7E8294" })
+
+vim.api.nvim_set_hl(0, "CmpItemKindUnit", { fg = "#F5EBD9", bg = "#D4A959" })
+vim.api.nvim_set_hl(0, "CmpItemKindSnippet", { fg = "#F5EBD9", bg = "#D4A959" })
+vim.api.nvim_set_hl(0, "CmpItemKindFolder", { fg = "#F5EBD9", bg = "#D4A959" })
+
+vim.api.nvim_set_hl(0, "CmpItemKindMethod", { fg = "#DDE5F5", bg = "#6C8ED4" })
+vim.api.nvim_set_hl(0, "CmpItemKindValue", { fg = "#DDE5F5", bg = "#6C8ED4" })
+vim.api.nvim_set_hl(0, "CmpItemKindEnumMember", { fg = "#DDE5F5", bg = "#6C8ED4" })
+
+vim.api.nvim_set_hl(0, "CmpItemKindInterface", { fg = "#D8EEEB", bg = "#58B5A8" })
+vim.api.nvim_set_hl(0, "CmpItemKindColor", { fg = "#D8EEEB", bg = "#58B5A8" })
+vim.api.nvim_set_hl(0, "CmpItemKindTypeParameter", { fg = "#D8EEEB", bg = "#58B5A8" })
