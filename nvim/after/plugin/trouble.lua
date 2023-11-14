78c8be64f591d58d46f9d2a8f80075e2ed9b9277
Myzel394 <50424412+Myzel394@users.noreply.github.com>
2023-11-14 19:57:41 +0100

    fix: Trying to fix latex

--- nvim/after/plugin/trouble.lua
+++ nvim/after/plugin/trouble.lua
@@ -1,1 +1,1 @@
-vim.keymap.set("n", "<leader>tt", "<cmd>TroubleToggle<cr>", { silent = true, desc = "Toggle trouble menu" })
+vim.keymap.set("n", "<leader>r", "<cmd>TroubleToggle<cr>", { silent = true, desc = "Toggle trouble menu" })
