2d26e5ad134df05265dae9c3d052e02158963efd
Myzel394 <50424412+Myzel394@users.noreply.github.com>
2023-12-20 14:42:57 +0100

    debug: Adding download bing wallpaper

---
+++ scripts/download_bing_wallpaper_macos.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+function wallpaper() { 
+    automator -i "${1}" /Users/myzel394/Desktop/setDesktop.workflow
+}
+
+echo "Creating folder"
+# Create the folder if it doesn't exist
+folder=~/Pictures/BingWallpapers
+mkdir -p $folder
+
+echo "Getting url"
+# Get the Bing picture of the day
+wallpaper_path=$(curl 'https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=en-US' -s | rg '(?<="url":")(.*)(?=","urlbase")' -or '$1' --pcre2)
+wallpaper_url="https://www.bing.com/$wallpaper_path"
+
+# Get the filename
+date_formatted=$(date -u +%Y%m%d)
+filename="$date_formatted-dummyname.jpg"
+
+echo "Downloading wallpaper to $folder/$filename"
+# Download the wallpaper
+wget -O $folder/$filename $wallpaper_url -q
+
+echo "Setting wallpaper"
+wallpaper $folder/$filename
+
---
+++ scripts/set_background.scpt
@@ -0,0 +1,5 @@
+tell application "System Events"
+    tell every desktop
+        set picture to "/Users/myzel394/Pictures/BingWallpapers/20231218-dummyname.jpg"
+    end tell
+end tell
