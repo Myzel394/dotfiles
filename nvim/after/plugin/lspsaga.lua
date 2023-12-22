a031e011a1f31473245d63165255056f54366e85
Myzel394 <50424412+Myzel394@users.noreply.github.com>
2023-12-22 10:01:50 +0100

    fix: Improve remaps

--- nvim/after/plugin/lspsaga.lua
+++ nvim/after/plugin/lspsaga.lua
@@ -1,8 +1,29 @@
-vim.keymap.set("n", "<leader>la", function() vim.cmd("Lspsaga code_action") end,
-    { noremap = true, silent = true, desc = "Code Action (LSPSaga)" })
-vim.keymap.set("n", "<leader>lr", function() vim.cmd("Lspsaga rename") end,
-    { noremap = true, silent = true, desc = "Rename (LSPSaga)" })
-vim.keymap.set("n", "<leader>lp", function() vim.cmd("Lspsaga peek_definition") end,
-    { noremap = true, silent = true, desc = "Peek Definition (LSPSaga)" })
-vim.keymap.set("n", "<leader>lg", function() vim.cmd("Lspsaga goto_definition") end,
-    { noremap = true, silent = true, desc = "Go to Definition (LSPSaga)" })
+vim.keymap.set(
+    "n",
+    "<leader>la",
+    function() vim.cmd("Lspsaga code_action") end,
+    { noremap = true, silent = true, desc = "Code Action (LSPSaga)" }
+)
+vim.keymap.set(
+    "n",
+    "<leader>lr",
+    function() vim.cmd("Lspsaga rename") end,
+    { noremap = true, silent = true, desc = "Rename (LSPSaga)" }
+)
+vim.keymap.set(
+    "n",
+    "<leader>lp",
+    function() vim.cmd("Lspsaga peek_definition") end,
+    { noremap = true, silent = true, desc = "Peek Definition (LSPSaga)" }
+)
+vim.keymap.set(
+    "n",
+    "<leader>lg",
+    function() vim.cmd("Lspsaga goto_definition") end,
+    { noremap = true, silent = true, desc = "Go to Definition (LSPSaga)" }
+)
+vim.keymap.set(
+    {"n", "i"},
+    "<S-Space>", function() vim.cmd("Lspsaga hover_doc") end,
+    { noremap = true, silent = true, desc = "Hover Doc (LSPSaga)" }
+)
