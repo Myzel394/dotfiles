fc069b21fbc6a97d1df562550cae551283c29823
Myzel394 <50424412+Myzel394@users.noreply.github.com>
2023-12-23 19:01:06 +0100

    fix(scripts): Fix settings wallpapers on MacOS

--- scripts/download_bing_wallpaper_macos.sh
+++ scripts/download_bing_wallpaper_macos.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 
 function wallpaper() { 
-    automator -i "${1}" /Users/myzel394/Desktop/setDesktop.workflow
+    desktoppr "$1"
 }
 
 echo "Creating folder"
 # Create the folder if it doesn't exist
-folder=~/Pictures/BingWallpapers
+folder=~/Pictures/BingWallpaper
 mkdir -p $folder
 
 echo "Getting url"
 # Get the Bing picture of the day
 wallpaper_path=$(curl 'https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=en-US' -s | rg '(?<="url":")(.*)(?=","urlbase")' -or '$1' --pcre2)
 wallpaper_url="https://www.bing.com/$wallpaper_path"
+echo "Found url: $wallpaper_url"
 
 # Get the filename
 date_formatted=$(date -u +%Y%m%d)
