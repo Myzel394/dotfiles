bdba187ac9b865eafa1d49adb0f621136223f254
Myzel394 <50424412+Myzel394@users.noreply.github.com>
2024-01-03 16:25:33 +0100

    feat(nvim): Add better undotree

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -1,14 +1,20 @@
 local builtin = require("telescope.builtin")
 
-require("telescope").setup()
+require("telescope").setup {
+    extensions = {
+        undo = {}
+    }
+}
 require("telescope").load_extension("yank_history")
+require("telescope").load_extension("undo")
 
 vim.keymap.set("n", "<leader>f", builtin.find_files, { desc = "Find files" })
 vim.keymap.set("n", "<leader>s", builtin.live_grep, { desc = "Find files with live grep" })
 vim.keymap.set("n", "<leader>ss", builtin.grep_string, { desc = "Find files with live grep" })
-vim.keymap.set("n", "<leader>b", builtin.buffers, { desc = "Show buffers" })
-vim.keymap.set("n", "<leader>t", builtin.treesitter, { desc = "Show treesitter" })
 
+vim.keymap.set("n", "<leader>cb", builtin.buffers, { desc = "Show buffers" })
+vim.keymap.set("n", "<leader>ct", builtin.treesitter, { desc = "Show treesitter" })
+vim.keymap.set("n", "<leader>cu", "<cmd>Telescope undo<cr>", { desc = "Show undo history" })
 vim.keymap.set("n", "<leader>cs", function() vim.cmd("Telescope git_status") end, { desc = "Show git status" })
 vim.keymap.set("n", "<leader>cb", function() vim.cmd("Telescope git_branches") end, { desc = "Show git branches" })
 vim.keymap.set("n", "<leader>cc", function() vim.cmd("Telescope git_commits") end, { desc = "Show git commits" })
--- nvim/after/plugin/tokyonight.lua
+++ nvim/after/plugin/tokyonight.lua
@@ -4,46 +4,6 @@
             light_style = "day",
             transparent = true,
             terminal_colors = true,
-            styles = {
-                comments = { italic = true },
-                keywords = { italic = true },
-                functions = {},
-                variables = {},
-                sidebars = "dark",
-                floats = "dark",
-            },
-            sidebars = { "qf", "help" },
-            day_brightness = 0.3,
-            hide_inactive_statusline = false,
-            dim_inactive = false,
-            lualine_bold = false,
-
-            on_colors = function(colors)
-                colors.bg = "#11171f"
-                colors.bg_dark = "#2b3f4e"
-                colors.bg_float = "#1c2a33"
-                colors.bg_highlight = "#1c2a33"
-                colors.bg_popup = "#0e1419"
-                colors.bg_search = "#0e1419"
-                colors.bg_sidebar = "#000000"
-                colors.bg_statusline = "#000000"
-                colors.bg_visual = "#000000"
-
-                colors.cyan = "#044bc4"
-                colors.green = "#044bc4"
-                colors.green1 = "#044bc4"
-                colors.green2 = "#044bc4"
-                colors.magenta = "#044bc4"
-                colors.magenta2 = "#044bc4"
-                colors.orange = "#044bc4"
-                colors.purple = "#044bc4"
-                colors.red = "#044bc4"
-                colors.red1 = "#044bc4"
-                colors.teal = "#044bc4"
-                colors.yellow = "#044bc4"
-            end,
-
-            on_highlights = function(highlights, colors) end,
         })
         vim.cmd[[colorscheme tokyonight]]
     
