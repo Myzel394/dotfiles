587413b03254f32d31de801166e89e45c2cadd4d
Myzel394 <50424412+Myzel394@users.noreply.github.com>
2024-01-05 13:14:36 +0100

    feat(nvim): Improve lspsaga and tabby

--- nvim/after/plugin/lspsaga.lua
+++ nvim/after/plugin/lspsaga.lua
@@ -19,10 +19,36 @@ vim.keymap.set(
 vim.keymap.set(
     "n",
     "<leader>lg",
+    -- This function is quite hacky, if you know a better solution, please
+    -- let me know
     function()
         -- Run goto_definition in new tab
-        vim.cmd("tabnew")
+        -- Get current file + path
+        local buffer = vim.api.nvim_get_current_buf()
+        local filepath = vim.api.nvim_buf_get_name(0)
+        -- Get cursor
+        local cursor = vim.api.nvim_win_get_cursor(0)
+
         vim.cmd("Lspsaga goto_definition")
+
+        -- Wait for LSP to open new buffer
+        vim.wait(30, function() end)
+
+        local new_filepath = vim.api.nvim_buf_get_name(0)
+
+        if filepath ~= new_filepath then
+            local new_buffer = vim.api.nvim_get_current_buf()
+            local new_cursor = vim.api.nvim_win_get_cursor(0)
+
+            -- Set current buffer to previous file
+            vim.api.nvim_set_current_buf(buffer)
+            vim.api.nvim_win_set_cursor(0, cursor)
+
+            -- Open new tab
+            vim.cmd("tabnew")
+            vim.api.nvim_set_current_buf(new_buffer)
+            vim.api.nvim_win_set_cursor(0, new_cursor)
+        end
     end,
     { noremap = true, silent = true, desc = "Go to Definition (LSPSaga)" }
 )
