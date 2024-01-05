fedbb633b99e38977b329283134c3a01e8e9092b
Myzel394 <50424412+Myzel394@users.noreply.github.com>
2024-01-05 13:14:36 +0100

    feat(nvim): Improve lspsaga and tabby

--- nvim/after/plugin/tabby.lua
+++ nvim/after/plugin/tabby.lua
@@ -4,7 +4,7 @@ require('tabby.tabline').use_preset('active_wins_at_tail', {
 
 
 vim.keymap.set("n", "<leader>a", ":tab split<CR>", { desc = "Create new tab", noremap = true })
-vim.keymap.set("n", "<leader>d", ":tabclose<CR>", { desc = "Close this tab", noremap = true })
+vim.keymap.set("n", "<leader>tc", ":tabclose<CR>", { desc = "Close this tab", noremap = true })
 vim.keymap.set("n", "<leader>to", ":tabonly<CR>", { desc = "Close all other tabs", noremap = true })
 
 for i = 1, 10 do
