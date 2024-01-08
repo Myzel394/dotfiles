869278ec35f89b248021268a6511c5b8a0412767
Myzel394 <50424412+Myzel394@users.noreply.github.com>
2024-01-08 20:31:04 +0100

    feat(nvim): Add splitting per tab

---
+++ nvim/after/plugin/chatgpt.lua
@@ -0,0 +1,8 @@
+local home = vim.fn.expand("$HOME")
+
+require("chatgpt").setup({
+    api_key_cmd = "bash " .. home .. "/.config/nvim/scripts/extract-openai-api-key.sh",
+    openai_params = {
+        model = "gpt-4",
+    },
+})
--- nvim/after/plugin/tabby.lua
+++ nvim/after/plugin/tabby.lua
@@ -7,6 +7,23 @@ vim.keymap.set("n", "<leader>a", ":tab split<CR>", { desc = "Create new tab", no
 vim.keymap.set("n", "<leader>tc", ":tabclose<CR>", { desc = "Close this tab", noremap = true })
 vim.keymap.set("n", "<leader>to", ":tabonly<CR>", { desc = "Close all other tabs", noremap = true })
 
-for i = 1, 10 do
+for i = 1, 9 do
     vim.keymap.set("n", "<leader>" .. i, ":" .. i .. "tabnext<CR>", { desc = "Go to tab " .. i, noremap = true })
+end
+
+
+-- Open another tab in split
+for i = 1, 9 do
+    vim.keymap.set(
+        "n",
+        "<leader>ts" .. i,
+        function()
+            local win_id = vim.api.nvim_tabpage_get_win(i)
+            local buf = vim.api.nvim_win_get_buf(win_id)
+
+            vim.cmd("vert belowright sb" .. buf)
+            vim.cmd("tabclose " .. i)
+        end,
+        { desc = "Open tab " .. i .. " in vertical split" }
+    )
 end
