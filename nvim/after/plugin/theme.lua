76d985cba2024bb7d72d0ec03cd3bf1988fe8cb5
Myzel394 <50424412+Myzel394@users.noreply.github.com>
2024-02-06 20:55:23 +0100

    fix(nvim): Improve theme

--- nvim/after/plugin/theme.lua
+++ nvim/after/plugin/theme.lua
@@ -1,5 +1,3 @@
-vim.cmd.colorscheme("catppuccin")
-
 require("catppuccin").setup({
 	integrations = {
 		gitsigns = true,
@@ -20,21 +18,5 @@ require("catppuccin").setup({
 	},
 })
 
-vim.api.nvim_set_hl(0, "LeapBackdrop", { link = "Comment" })
-vim.api.nvim_set_hl(0, "LeapMatch", {
-	fg = "black",
-	bold = true,
-	nocombine = true,
-})
+vim.cmd.colorscheme("catppuccin")
 
-vim.api.nvim_set_hl(0, "LeapLabelPrimary", {
-	fg = "#ff40ff",
-	bold = true,
-	nocombine = true,
-})
-
-vim.api.nvim_set_hl(0, "LeapLabelSecondary", {
-	fg = "#8280ff",
-	bold = true,
-	nocombine = true,
-})
