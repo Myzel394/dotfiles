c3497908c432565ce82937eed6d02d4ba73214b0
Myzel394 <50424412+Myzel394@users.noreply.github.com>
2024-02-06 21:12:11 +0100

    fix(nvim): Improve leap and eyeliner

--- nvim/after/plugin/theme.lua
+++ nvim/after/plugin/theme.lua
@@ -20,3 +20,23 @@ require("catppuccin").setup({
 
 vim.cmd.colorscheme("catppuccin")
 
+vim.api.nvim_set_hl(0, "LeapBackdrop", { link = "Comment" })
+vim.api.nvim_set_hl(0, "LeapMatch", {
+	fg = "black",
+	bold = true,
+	nocombine = true,
+})
+
+vim.api.nvim_set_hl(0, "LeapLabelPrimary", {
+	fg = "#ff40ff",
+	bold = true,
+	nocombine = true,
+})
+
+vim.api.nvim_set_hl(0, "LeapLabelSecondary", {
+	fg = "#8280ff",
+	bold = true,
+	nocombine = true,
+})
+
+vim.api.nvim_set_hl(0, "EyelinerDimmed", { link = "Comment" })
