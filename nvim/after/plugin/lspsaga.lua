4438bd58a2bf9ec8595bda3bb18ef837478c77b3
Myzel394 <50424412+Myzel394@users.noreply.github.com>
2024-02-09 15:05:41 +0100

    fix!(nvim): Improve tabs; Update lspsaga

--- nvim/after/plugin/lspsaga.lua
+++ nvim/after/plugin/lspsaga.lua
@@ -26,47 +26,43 @@ vim.keymap.set(
     end,
     { noremap = true, silent = true, desc = "Peek Definition (LSPSaga)" }
 )
-vim.keymap.set(
-	"n",
-	"<leader>lg",
-	-- This function is quite hacky, if you know a better solution, please
-	-- let me know
-	function()
-		-- Run goto_definition in new tab
-		-- Get current file + path
-		local buffer = vim.api.nvim_get_current_buf()
-		local filepath = vim.api.nvim_buf_get_name(0)
-		-- Get cursor
-		local cursor = vim.api.nvim_win_get_cursor(0)
 
-		vim.cmd("Lspsaga goto_definition")
+vim.keymap.set("n", "<leader>lg", "<cmd>:Lspsaga goto_definition<CR>", { noremap = true, silent = true, desc = "Go to Definition (LSPSaga)" })
 
-		-- Wait for LSP to open new buffer
-		vim.wait(30, function() end)
-
-		local new_filepath = vim.api.nvim_buf_get_name(0)
-
-		if filepath ~= new_filepath then
-			local new_buffer = vim.api.nvim_get_current_buf()
-			local new_cursor = vim.api.nvim_win_get_cursor(0)
-
-			-- Set current buffer to previous file
-			vim.api.nvim_set_current_buf(buffer)
-			vim.api.nvim_win_set_cursor(0, cursor)
-
-			-- Open new tab
-			vim.cmd("tabnew")
-			vim.api.nvim_set_current_buf(new_buffer)
-			vim.api.nvim_win_set_cursor(0, new_cursor)
-		end
-	end,
-	{ noremap = true, silent = true, desc = "Go to Definition (LSPSaga)" }
-)
-vim.keymap.set(
-    { "n", "i" },
-    "<S-Space>",
-    function()
-        vim.cmd("Lspsaga hover_doc")
-    end,
-    { noremap = true, silent = true, desc = "Hover Doc (LSPSaga)" }
-)
+-- Use the below code to open the definition in a new tab
+-- vim.keymap.set(
+-- 	"n",
+-- 	"<leader>lg",
+-- 	-- This function is quite hacky, if you know a better solution, please
+-- 	-- let me know
+-- 	function()
+-- 		-- Run goto_definition in new tab
+-- 		-- Get current file + path
+-- 		local buffer = vim.api.nvim_get_current_buf()
+-- 		local filepath = vim.api.nvim_buf_get_name(0)
+-- 		-- Get cursor
+-- 		local cursor = vim.api.nvim_win_get_cursor(0)
+--
+-- 		vim.cmd("Lspsaga goto_definition")
+--
+-- 		-- Wait for LSP to open new buffer
+-- 		vim.wait(30, function() end)
+--
+-- 		local new_filepath = vim.api.nvim_buf_get_name(0)
+--
+-- 		if filepath ~= new_filepath then
+-- 			local new_buffer = vim.api.nvim_get_current_buf()
+-- 			local new_cursor = vim.api.nvim_win_get_cursor(0)
+--
+-- 			-- Set current buffer to previous file
+-- 			vim.api.nvim_set_current_buf(buffer)
+-- 			vim.api.nvim_win_set_cursor(0, cursor)
+--
+-- 			-- Open new tab
+-- 			vim.cmd("tabnew")
+-- 			vim.api.nvim_set_current_buf(new_buffer)
+-- 			vim.api.nvim_win_set_cursor(0, new_cursor)
+-- 		end
+-- 	end,
+-- 	{ noremap = true, silent = true, desc = "Go to Definition (LSPSaga)" }
+-- )
