9e26362b6b1cb715d975a834a2aec2a8cb245d37
Myzel394 <50424412+Myzel394@users.noreply.github.com>
2024-03-17 18:11:59 +0100

    feat(nvim): Add more icons to queries

--- nvim/queries/tsx/highlights.scm
+++ nvim/queries/tsx/highlights.scm
@@ -14,6 +14,39 @@
 
 ((identifier) @function.call
     (#eq? @function.call "useRef")
-    (#set! conceal "")
+    (#set! conceal "")
 )
 
+((identifier) @function.call
+    (#eq? @function.call "useCallback")
+    (#set! conceal "󱡁 ")
+)
+
+(jsx_attribute
+  (property_identifier) @tag.attribute
+  (#eq? @tag.attribute "className")
+  (#set! conceal "类")
+)
+
+(jsx_attribute
+  (property_identifier) @tag.attribute
+  (#eq? @tag.attribute "style")
+  (#set! conceal "󰌜 ")
+)
+
+(jsx_attribute
+  (property_identifier) @tag.attribute
+  (#eq? @tag.attribute "key")
+  (#set! conceal "󰌋 ")
+)
+
+; Match if attribute and value are equal
+; example: <div open={open} />
+(jsx_attribute
+  (property_identifier) @tag.attribute @key
+  (jsx_expression
+    (identifier) @variable @value
+    (#eq? @key @value)
+    (#set! conceal "")
+  )
+)
