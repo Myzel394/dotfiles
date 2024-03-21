3d12fc837619a18643ac60211fae01a5709473f7
Myzel394 <50424412+Myzel394@users.noreply.github.com>
2024-03-21 10:57:22 +0100

    feat(nvim): Add split buffer right and left functionality

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -2,58 +2,58 @@ local builtin = require("telescope.builtin")
 local telescope_actions = require("telescope.actions")
 
 local function send_to_quickfix(promtbufnr)
-	telescope_actions.smart_send_to_qflist(promtbufnr)
-	vim.cmd([[botright copen]])
+    telescope_actions.smart_send_to_qflist(promtbufnr)
+    vim.cmd([[botright copen]])
 end
 
 require("telescope").setup({
-	extensions = {
-		undo = {
-			use_delta = true,
-			side_by_side = false,
-			diff_context_lines = 10,
-		},
-		emoji = {
-			action = function(emoji)
-				-- argument emoji is a table.
-				-- {name="", value="", cagegory="", description=""}
+    extensions = {
+        undo = {
+            use_delta = true,
+            side_by_side = false,
+            diff_context_lines = 10,
+        },
+        emoji = {
+            action = function(emoji)
+                -- argument emoji is a table.
+                -- {name="", value="", cagegory="", description=""}
 
-				-- insert emoji when picked
-				vim.api.nvim_put({ emoji.value }, "c", false, true)
-			end,
-		},
-		frecency = {
-			ignore_patterns = { "*.git/*", "*/tmp/*", "*/node_modules/*", "*/.venv/*" },
-			db_root = vim.fn.stdpath("data") .. "/frecency",
-		},
-	},
-	defaults = {
-		layout_strategy = "vertical",
-		layout_config = {
-			mirror = true,
-			preview_height = 0.65,
-			prompt_position = "top",
-		},
-		mappings = {
-			["n"] = {
-				["<C-q>"] = send_to_quickfix,
-			},
-			["i"] = {
-				["<C-q>"] = send_to_quickfix,
-			},
-		},
-		vimgrep_arguments = {
-			"rg",
-			"--color=never",
-			"--no-heading",
-			"--with-filename",
-			"--line-number",
-			"--column",
-			"--smart-case",
-			-- Own additions
-			"--trim",
-		},
-	},
+                -- insert emoji when picked
+                vim.api.nvim_put({ emoji.value }, "c", false, true)
+            end,
+        },
+        frecency = {
+            ignore_patterns = { "*.git/*", "*/tmp/*", "*/node_modules/*", "*/.venv/*" },
+            db_root = vim.fn.stdpath("data") .. "/frecency",
+        },
+    },
+    defaults = {
+        layout_strategy = "vertical",
+        layout_config = {
+            mirror = true,
+            preview_height = 0.65,
+            prompt_position = "top",
+        },
+        mappings = {
+            ["n"] = {
+                ["<C-q>"] = send_to_quickfix,
+            },
+            ["i"] = {
+                ["<C-q>"] = send_to_quickfix,
+            },
+        },
+        vimgrep_arguments = {
+            "rg",
+            "--color=never",
+            "--no-heading",
+            "--with-filename",
+            "--line-number",
+            "--column",
+            "--smart-case",
+            -- Own additions
+            "--trim",
+        },
+    },
 })
 -- require("telescope").load_extension("frecency")
 require("telescope").load_extension("yank_history")
@@ -65,8 +65,6 @@ vim.keymap.set("n", "<leader>f", "<cmd>Telescope frecency<cr>", { desc = "Find f
 vim.keymap.set("n", "<leader>i", builtin.find_files, { desc = "Find files" })
 vim.keymap.set("n", "<leader>s", builtin.live_grep, { desc = "Find files with live grep" })
 vim.keymap.set("n", "<leader>w", builtin.resume, { desc = "Resume your last search" })
-vim.keymap.set("n", "<leader>b", builtin.buffers, { desc = "Open buffers" })
-vim.keymap.set("n", "<leader>ss", builtin.grep_string, { desc = "Find files with live grep" })
 
 vim.keymap.set("n", "<leader>cb", builtin.buffers, { desc = "Show buffers" })
 vim.keymap.set("n", "<leader>ct", builtin.treesitter, { desc = "Show treesitter" })
@@ -77,3 +75,63 @@ vim.keymap.set("n", "<leader>cb", "<cmd>Telescope git_branches<cr>", { desc = "S
 vim.keymap.set("n", "<leader>cc", "<cmd>Telescope git_commits<cr>", { desc = "Show git commits" })
 vim.keymap.set("n", "<leader>cy", "<cmd>Telescope yank_history<cr>", { desc = "Show yank history" })
 vim.keymap.set("n", "<leader>ce", "<cmd>Telescope emoji<cr>", { desc = "Show emoji selection" })
+
+local function get_visible_buffers()
+    return vim.tbl_filter(function(bufnr)
+        if 1 ~= vim.fn.buflisted(bufnr) then
+            return false
+        end
+
+        if not vim.api.nvim_buf_is_loaded(bufnr) then
+            return false
+        end
+
+        return true
+    end, vim.api.nvim_list_bufs())
+end
+
+
+vim.keymap.set(
+    "n",
+    "<leader>bsr",
+    function()
+        local available_bufs = get_visible_buffers()
+        local current_buf = vim.api.nvim_get_current_buf()
+
+        local right_buf = nil
+
+        for i, bufnr in ipairs(available_bufs) do
+            if bufnr == current_buf then
+                right_buf = available_bufs[i + 1]
+                break
+            end
+        end
+
+        if right_buf then
+            vim.cmd("vert belowright sb " .. right_buf)
+        end
+    end,
+    { desc = "Split right buffer vertically" }
+)
+vim.keymap.set(
+    "n",
+    "<leader>bsl",
+    function()
+        local available_bufs = get_visible_buffers()
+        local current_buf = vim.api.nvim_get_current_buf()
+
+        local left_buf = nil
+
+        for i, bufnr in ipairs(available_bufs) do
+            if bufnr == current_buf then
+                left_buf = available_bufs[i - 1]
+                break
+            end
+        end
+
+        if left_buf then
+            vim.cmd("vert belowright sb " .. left_buf)
+        end
+    end,
+    { desc = "Split left buffer vertically" }
+)
