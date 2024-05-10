33a361bb1e64186c0f83037bfd1acd9cc9ce9194
Myzel394 <50424412+Myzel394@users.noreply.github.com>
2024-05-10 23:15:11 +0200

    feat(tmux): Improve timetrap; improve theme

--- tmux/timetrap.sh
+++ tmux/timetrap.sh
@@ -2,9 +2,18 @@
 
 current_time=$(echo $(t now))
 
+if [[ $current_time == "No running entries" ]]; then
+    echo "󱑁 Not tracking"
+    exit 0
+fi
+
 sheet=$(echo $current_time | cut -d ' ' -f 5)
-time=$(echo $current_time | cut -d ' ' -f 6)
+time=$(echo $current_time | cut -d ' ' -f 6 | cut -d ':' -f -2)
 note=$(echo $current_time | cut -d ' ' -f 7)
 
-echo "󱑁 $time ($note)  $sheet"
+if [[ "$note" == "" ]]; then
+    echo "󱑁 $time"
+else
+    echo "󱑁 $time ($note)"
+fi
 
