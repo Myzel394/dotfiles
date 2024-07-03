8160edb9c8295d2efa285c8256ca2668e1464f9b
Myzel394 <50424412+Myzel394@users.noreply.github.com>
2024-07-03 12:05:37 +0200

    chore(nvim): Update dependencies; Improve for low running hardware

--- nvim/after/plugin/chatgpt.lua
+++ nvim/after/plugin/chatgpt.lua
@@ -1,3 +1,9 @@
+local IS_RUNNING_ON_LIMITED_HARDWARE = os.getenv("DOTFILES_RUNNING_ON_LIMITED_HARDWARE") == "1"
+
+if IS_RUNNING_ON_LIMITED_HARDWARE then
+	return
+end
+
 local home = vim.fn.expand("$HOME")
 
 require("chatgpt").setup({
