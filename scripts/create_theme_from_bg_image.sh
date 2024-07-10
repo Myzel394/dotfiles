8c772a6dd922a17bf3e3cbe5b3c9f6c3a3fcc88c
Myzel394 <50424412+Myzel394@users.noreply.github.com>
2024-07-10 17:57:04 +0200

    fix(scripts): Fix folder

--- scripts/create_theme_from_bg_image.sh
+++ scripts/create_theme_from_bg_image.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 FOLDER=~/Pictures/BingWallpapers
+
+if ! [[ -d "$FOLDER" ]]; then
+    FOLDER=~/Pictures/BingWallpaper
+fi
+
 # Finds the most recent image in the folder
 IMAGE=$(ls "$FOLDER" | sort -k 1,1 -t"-" -n --reverse | head -n 1)
 
