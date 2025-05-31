cccb3e3c1ef372dd74fc780e3bde5bc4862c04d5
Myzel394 <github.7a2op@simplelogin.co>
2025-05-31 11:24:36 +0200

    fix: Fixes

---
+++ scripts/download_theme_hyprpaper.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+function wallpaper() { 
+    hyprpaper set "$1"
+}
+
+echo "Creating folder"
+# Create the folder if it doesn't exist
+folder=~/Pictures/BingWallpapers
+mkdir -p $folder
+
+echo "Getting url"
+# Get the Bing picture of the day
+wallpaper_path=$(curl 'https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=en-US' -s | rg '(?<="url":")(.*)(?=","urlbase")' -or '$1' --pcre2)
+wallpaper_url="https://www.bing.com/$wallpaper_path"
+echo "Found url: $wallpaper_url"
+
+# Get the filename
+date_formatted=$(date -u +%Y%m%d)
+filename="current-image.jpg"
+
+echo "Downloading wallpaper to $folder/$filename"
+# Download the wallpaper
+wget -O $folder/$filename $wallpaper_url -q
+
